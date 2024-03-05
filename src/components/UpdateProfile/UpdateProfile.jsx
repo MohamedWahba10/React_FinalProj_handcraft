02c9b819f7353dd9b2d5e036a62bda82c07caf62
Merna Bassem <mernabassem21@gmail.com>
2024-03-05 09:02:04 +0200

    change dessign

--- src/components/UpdateProfile/UpdateProfile.jsx
+++ src/components/UpdateProfile/UpdateProfile.jsx
@@ -1,228 +1,7 @@
-// import React, { useContext, useEffect, useState } from "react";
-// import styles from "./UpdateProfile.module.css";
-// import PersonIcon from "../../assets/images/personIcon.jpeg";
-// import axios from "axios";
-// import { jwtDecode } from "jwt-decode";
-// import * as Yup from "yup";
-// import { Link, useNavigate } from "react-router-dom";
-// import { Helmet } from "react-helmet";
-// import { useFormik } from "formik";
-// import { TokenContext } from "../../Context/Token";
-
-// export default function UpdateProfile() {
-//   let navigate = useNavigate();
-//   const [error, setError] = useState(null);
-//   const [isLoading, setIsLoading] = useState(false);
-//   let { setToken, setUserData, userData } = useContext(TokenContext);
-//   const userToken = localStorage.getItem("userToken");
-//   var userDataString = localStorage.getItem("userData");
-//   userData = JSON.parse(userDataString);
-//   // console.log("userData is ............. ", userData);
-//   const userId = jwtDecode(userToken).id; 
-//   console.log(userId)
-//   async function callUpdateProfile(userId,userData) {
-//     setError("");
-//     console.log("Merrrrrrna")
-//     setIsLoading(true);
-//     try {
-//       const response = await axios.put(
-//         `http://localhost:8000/api/${userId}/`, 
-//         userData 
-//       );
-//       console.log(response);
-     
-//       if (response.data.message === "Data Updated Successfully") {
-//         navigate("/");
-//         setUserData(response.data)
-//       } else {
-//         console.log("data", response.data.message);
-//       }
-      
-//     } catch (error) {
-//       setError(error.response.data.detail);
-//       setIsLoading(false);
-//       console.error("Error during Update:", error.response.data.detail);
-//     }
-//   }
-
-//   const validationSchema = Yup.object({
-//     first_name: Yup.string()
-//       .matches(/^[a-zA-Z]{3,10}$/, "name must be from 3 to 10 letters")
-//       .required("Required"),
-//     last_name: Yup.string()
-//       .matches(/^[a-zA-Z]{3,10}$/, "name must be from 3 to 10 letters")
-//       .required("Required"),
-//     phone: Yup.string().matches(/^01[1250][0-9]{8}$/, "invalid"),
-//     address: Yup.string().matches(
-//       /^[a-zA-Z]{3,35}$/,
-//       "Address must be from 3 to 35 letters"
-//     ),
-//   });
-
-//   const updateProfile = useFormik({
-//     initialValues: {
-//       first_name: userData.first_name,
-//       email: userData.email,
-//       password: userData.password,
-//       last_name: userData.last_name,
-//       phone: userData.phone,
-//       address: userData.address,
-//       usertype: userData.usertype,
-//       ssn: userData.ssn,
-//       is_superuser: userData.is_superuser,
-//       shopname: userData.is_superuser,
-//     },
-//     validationSchema,
-//     onSubmit: callUpdateProfile,
-//   });
-
-//   return (
-//     <>
-//       <Helmet>
-//         <title>Update Profile</title>
-//       </Helmet>
-//       <div className={`${styles.header_update} py-5 mb-5 text-center `}>
-//         <h1>Update Profile</h1>
-
-//         <Link to="/profile" className="text-decoration-none ">
-//           <span className={`${styles.link_home} pe-1 `}>Your Profile</span>
-//         </Link>
-
-//         <span className={`${styles.span_update}`}>&gt; Update Profile</span>
-//       </div>
-//       <div className="container my-5 py-5">
-//         <div>
-//           <div className={`${styles.form_update}`}>
-//             <h2 className="text-center py-3">Update Profile</h2>
-//             {error ? <div className="alert alert-danger"> {error}</div> : ""}
-//             <form onSubmit={updateProfile.handleSubmit}>
-//               <div className="row gy-4">
-//                 <div className="col-md-6">
-//                   <div className="form-group">
-//                     <label htmlFor="first_name" className="fs-4 fw-bold">
-//                       First Name
-//                     </label>
-
-//                     <input
-//                       type="text"
-//                       className="w-100"
-//                       id="first_name"
-//                       value={updateProfile.values.first_name}
-//                       name="first_name"
-//                       placeholder="Enter The First Name "
-//                       onChange={updateProfile.handleChange}
-//                       onBlur={updateProfile.handleBlur}
-//                     />
-//                     {updateProfile.errors.first_name &&
-//                     updateProfile.touched.first_name ? (
-//                       <div className="text-danger fs-5 mt-3">
-//                         {updateProfile.errors.first_name}
-//                       </div>
-//                     ) : null}
-//                   </div>
-//                 </div>
-//                 <div className="col-md-6">
-//                   <div className="form-group">
-//                     <label htmlFor="last_name" className="fs-4 fw-bold">
-//                       Last Name
-//                     </label>
-//                     <input
-//                       type="text"
-//                       className="w-100 "
-//                       id="last_name"
-//                       value={updateProfile.values.last_name}
-//                       name="last_name"
-//                       placeholder="Enter The Last Name"
-//                       onChange={updateProfile.handleChange}
-//                       onBlur={updateProfile.handleBlur}
-//                     />
-//                     {updateProfile.errors.last_name &&
-//                     updateProfile.touched.last_name ? (
-//                       <div className="text-danger fs-5 mt-3">
-//                         {updateProfile.errors.last_name}
-//                       </div>
-//                     ) : null}
-//                   </div>
-//                 </div>
-//                 <div className="col-md-6">
-//                   <div className="form-group">
-//                     <label htmlFor="phone" className="fs-4 fw-bold">
-//                       Phone
-//                     </label>
-
-//                     <input
-//                       type="text"
-//                       className="w-100"
-//                       id="phone"
-//                       value={updateProfile.values.phone}
-//                       name="phone"
-//                       placeholder="Enter The Phone "
-//                       onChange={updateProfile.handleChange}
-//                       onBlur={updateProfile.handleBlur}
-//                     />
-//                     {updateProfile.errors.phone &&
-//                     updateProfile.touched.phone ? (
-//                       <div className="text-danger fs-5 mt-3">
-//                         {updateProfile.errors.phone}
-//                       </div>
-//                     ) : null}
-//                   </div>
-//                 </div>
-//                 <div className="col-md-6">
-//                   <div className="form-group">
-//                     <label htmlFor="address" className="fs-4 fw-bold">
-//                       Address
-//                     </label>
-
-//                     <input
-//                       type="text"
-//                       className="w-100"
-//                       id="address"
-//                       value={updateProfile.values.address}
-//                       name="address"
-//                       placeholder="Enter The Address "
-//                       onChange={updateProfile.handleChange}
-//                       onBlur={updateProfile.handleBlur}
-//                     />
-//                     {updateProfile.errors.address &&
-//                     updateProfile.touched.address ? (
-//                       <div className="text-danger fs-5 mt-3">
-//                         {updateProfile.errors.address}
-//                       </div>
-//                     ) : null}
-//                   </div>
-//                 </div>
-//               </div>
-
-//               <div className={`d-flex justify-content-between my-3`}>
-//                 {isLoading ? (
-//                   <button type="submit" className={`${styles.update_button}`}>
-//                     <i className="fa fa-spinner fa-spin"></i>
-//                   </button>
-//                 ) : (
-//                   <button
-//                     type="submit"
-//                     className={`${styles.update_button}`}
-//                     disabled={!(updateProfile.isValid && updateProfile.dirty)}
-//                   >
-//                     Save Change
-//                   </button>
-//                 )}
-//               </div>
-//             </form>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-
 import React, { useContext, useEffect, useState } from "react";
 import styles from "./UpdateProfile.module.css";
-// import PersonIcon from "../../assets/images/personIcon.jpeg";
 import axios from "axios";
-import {jwtDecode} from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import * as Yup from "yup";
 import { Link, useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet";
@@ -238,29 +17,26 @@ export default function UpdateProfile() {
   var userDataString = localStorage.getItem("userData");
   userData = JSON.parse(userDataString);
 
-  async function callUpdateProfile(values) { 
-
-    console.log("values",values)
-    const userId = jwtDecode(userToken).id; 
+  async function callUpdateProfile(values) {
+    console.log("values", values);
+    const userId = jwtDecode(userToken).id;
     setError("");
-    console.log("Merrrrrrna")
+    console.log("Merrrrrrna");
     setIsLoading(true);
     try {
       const response = await axios.put(
-        `http://localhost:8000/api/${userId}/`, 
+        `http://localhost:8000/api/${userId}/`,
         values
       );
       console.log(response);
-     
+
       if (response.data.message === "Data Updated Successfully") {
         navigate("/");
-        setUserData(response.data.user)
+        setUserData(response.data.user);
         localStorage.setItem("userData", JSON.stringify(response.data.user));
-
       } else {
         console.log("data", response.data.message);
       }
-      
     } catch (error) {
       setError(error.response.data.detail);
       setIsLoading(false);
@@ -277,6 +53,14 @@ export default function UpdateProfile() {
       .required("Required"),
     phone: Yup.string().matches(/^01[1250][0-9]{8}$/, "invalid"),
     address: Yup.string(),
+    ssn: Yup.string().when("usertype", {
+      is: "vendor",
+      then: () =>
+        Yup.string()
+          .matches(/^\d{14}$/, "SSN must be a 14-digit number")
+          .required("SSN is required"),
+      otherwise: () => Yup.string().notRequired(),
+    }),
   });
 
   const updateProfile = useFormik({
@@ -291,7 +75,6 @@ export default function UpdateProfile() {
       ssn: userData.ssn,
       shopname: userData.shopname,
       is_superuser: userData.is_superuser,
-
     },
     validationSchema,
     onSubmit: (values) => callUpdateProfile(values),
@@ -413,7 +196,31 @@ export default function UpdateProfile() {
                     ) : null}
                   </div>
                 </div>
-                
+                {userData.usertype === "customer" ? null : (
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label htmlFor="ssn" className="fs-4 fw-bold">
+                        SSN
+                      </label>
+
+                      <input
+                        type="text"
+                        className="w-100"
+                        id="ssn"
+                        value={updateProfile.values.ssn}
+                        name="ssn"
+                        placeholder="Enter The SSN "
+                        onChange={updateProfile.handleChange}
+                        onBlur={updateProfile.handleBlur}
+                      />
+                      {updateProfile.errors.ssn && updateProfile.touched.ssn ? (
+                        <div className="text-danger fs-5 mt-3">
+                          {updateProfile.errors.ssn}
+                        </div>
+                      ) : null}
+                    </div>
+                  </div>
+                )}
               </div>
 
               <div className={`d-flex justify-content-between my-3`}>
